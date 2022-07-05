9cc72fda65667b33936e35987b1ad23bc2a9c62c
rasaqsodiq1221 <rasaqsodiq1221@gmail.com>
2022-07-05 17:46:36 +0100

    running debugging

---
+++ javascript_fundamentals/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/* function testingCodeRunner(app) {
+    if (app === 'coderunner') {
+        return `the ${app} runs perfectly `
+    }else {
+        return "something is still working"
+    }
+}
+
+console.log(testingCodeRunner("codeRunner")) */
+
+function welcomeToVscode() {
+    console.log(helloWorld())
+}
+
+function helloWorld() {
+    return "hello world"
+}
+
+welcomeToVscode();
